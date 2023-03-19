a91c7992ebb6ecb1eb6c4e7305e56df660179d89
jimmyarbats <jimmyaraujob@gmail.com>
2023-03-19 13:05:21 -0300

    done: req 03

---
+++ src/routes/loginRouter.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const generateToken = require('../utils/tokenGenerator');
+
+const router = express.Router();
+
+router.use(express.json());
+
+router.post('/', (req, res) => {
+  const token = generateToken();
+
+  return res.status(200).json({ token });
+});
+
+module.exports = router;
