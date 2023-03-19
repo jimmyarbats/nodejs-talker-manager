191d4548c1395e933b3a07d8fbb392fa682f4888
jimmyarbats <jimmyaraujob@gmail.com>
2023-03-19 13:23:04 -0300

    done: req 04

--- src/routes/loginRouter.js
+++ src/routes/loginRouter.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const generateToken = require('../utils/tokenGenerator');
 
+const validateEmail = require('../middlewares/validateEmail');
+const validatePass = require('../middlewares/validatePass');
+
 const router = express.Router();
 
 router.use(express.json());
 
-router.post('/', (req, res) => {
+router.post('/', validateEmail, validatePass, (req, res) => {
   const token = generateToken();
 
   return res.status(200).json({ token });
